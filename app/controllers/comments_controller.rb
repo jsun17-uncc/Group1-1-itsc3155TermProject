521a47bbb90745f44cde062755a90131784d2947
Joshua Sun <jsun17@uncc.edu>
2018-11-15 17:19:11 +0000

    Added comments controller, RailsBasics 8 - slide 22

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+    def create
+        @tip = Tip.find(params[:tip_id])
+        @comment = @tip.comments.create(comment_params)
+        redirect_to tip_path(@tip)
+    end 
+    
+    private
+        def comment_params
+            params.require(:comment).permit(:commenter, :body)
+        end 
+        
+end
