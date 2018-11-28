95c2edce68a529a890d489db43f1bcbaf1eab9ab
Reginald Harper <rharpe10@uncc.edu>
2018-11-28 21:23:31 +0000

    Created comments for all controllers and for tips views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
+#this controller regulates how the comments are handled
 class CommentsController < ApplicationController
+    
+    # method for creating a comment on a tip
     def create
         @tip = Tip.find(params[:tip_id])
         @comment = @tip.comments.create(comment_params)
         redirect_to tip_path(@tip)
     end 
     
+    # method for deleting a comment on a tip
     def destroy
         @tip = Tip.find(params[:tip_id])
         @comment = @tip.comments.find(params[:id])
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,12 +1,17 @@
+# this controller handles how tips within the application are handled
 class TipsController < ApplicationController
+    
+    # displays all tips in the database
     def index
         @tips = Tip.all
     end
     
+    # method to take the user to the new tip form
     def new
         @tip = Tip.new
     end
     
+    # takes the parameters passed in and creates a tip based on them
     def create
         @tip = Tip.new(tip_params)
         if @tip.save
@@ -16,14 +21,17 @@ class TipsController < ApplicationController
         end
     end
     
+    # method to find the specified tip and show it
     def show
         @tip = Tip.find(params[:id])
     end
     
+    # method to take the user to the tip edit page
     def edit
         @tip = Tip.find(params[:id])
     end
     
+    #takes the parameters and updates the specified tip
     def update
         @tip = Tip.find(params[:id])
         if @tip.update(tip_params)
@@ -33,6 +41,7 @@ class TipsController < ApplicationController
         end
     end
     
+    # method to destroy the specified tip
     def destroy
         @tip = Tip.find(params[:id])
         @tip.destroy
